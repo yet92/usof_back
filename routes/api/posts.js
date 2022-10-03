802e1216564c2b4ba182a6ce7436384db1944636
yet <zyzy92187@gmail.com>
2022-10-03 21:25:39 +0300

    add code for posts api. add NeedAuthorization error type. delete params from PostsService function. add toogleVisibility function to PostsService

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,13 +1,291 @@
-
 const {Router} = require('express');
 
 class PostsAPI {
 
-    constructor() {
-
+    /**
+     *
+     * @param {import('lib/services/PostsService')} postsService
+     * @param user
+     * @param checkAuthValidity
+     * @param admin
+     */
+    constructor(postsService, {user, checkAuthValidity, admin}) {
         this.router = Router();
+        this.postsService = postsService;
+
+        this.router.get('/',
+            user,
+            checkAuthValidity,
+            admin,
+            this.getPosts.bind(this)
+        )
+
+        this.router.get('/:id',
+            user,
+            checkAuthValidity,
+            admin,
+            async (req, res, next) => {
+
+                const id = req.params.id;
+                const userRole = req.isAdmin ? 'admin' : 'user';
+
+                try {
+                    res.json({
+                        post: await this.postsService.getPost(
+                            id,
+                            userRole,
+                            req.user?.id,
+                        )
+                    })
+                } catch (err) {
+                    next(err);
+                }
 
 
+            });
+
+        this.router.get('/:id/comments',
+            user,
+            checkAuthValidity,
+            admin,
+            async (req, res, next) => {
+                const id = req.params.id;
+                const userRole = req.isAdmin ? 'admin' : 'user';
+
+                try {
+                    res.json({
+                        comments: await this.postsService.getComments(
+                            id,
+                            userRole,
+                            req.user?.id,
+                        )
+                    })
+                } catch (err) {
+                    next(err);
+                }
+
+
+            });
+
+        this.router.get('/:id/categories',
+            user,
+            checkAuthValidity,
+            admin,
+            async (req, res, next) => {
+                const id = req.params.id;
+                const userRole = req.isAdmin ? 'admin' : 'user';
+
+                try {
+
+                    res.json({
+                        comments: await this.postsService.getCategories(
+                            id,
+                            userRole,
+                            req.user?.id || -1,
+                        )
+                    })
+                } catch (err) {
+                    next(err);
+                }
+
+            });
+
+        this.router.get('/:id/like',
+            user,
+            checkAuthValidity,
+            admin,
+            async (req, res, next) => {
+
+                const id = req.params.id;
+                const userRole = req.isAdmin ? 'admin' : 'user';
+
+                try {
+
+                    res.json({
+                        likes: await this.postsService.getLikes(
+                            id,
+                            userRole,
+                            req.user?.id || -1,
+                        )
+                    })
+                } catch (err) {
+                    next(err);
+                }
+            }
+        );
+
+        this.router.post('/:id/comments',
+            user,
+            checkAuthValidity,
+            async (req, res, next) => {
+                const id = req.params.id;
+                const content = req.body.content;
+
+                try {
+                    await this.postsService.addComment(id, req.user?.id, content)
+
+                    res.status(201).json({
+                        message: 'Comment created'
+                    })
+                } catch (err) {
+                    next(err);
+                }
+
+            })
+
+        this.router.post('/',
+            user,
+            checkAuthValidity,
+            async (req, res, next) => {
+
+                const {title, content, categoryIds} = req.body;
+
+                try {
+
+                    await this.postsService.createPost(
+                        req.user?.id,
+                        {title, content, categoryIds}
+                    )
+
+                    res.status(201).json({
+                        message: 'Post created'
+                    })
+
+                } catch (err) {
+                    next(err);
+                }
+
+            })
+
+        this.router.post('/:id/like',
+            user,
+            checkAuthValidity,
+            async (req, res, next) => {
+
+                const id = req.params.id;
+                const likeType = req.body.likeType;
+
+                try {
+
+                    await this.postsService.addLike(
+                        id,
+                        req.user?.id,
+                        likeType
+                    )
+
+                } catch (err) {
+                    next(err);
+                }
+
+            });
+
+        this.router.patch('/:id',
+            user,
+            checkAuthValidity,
+            async (req, res, next) => {
+
+                const id = req.params.id;
+                const {title, content, categoryIds} = req.body;
+
+                try {
+
+                    await this.postsService.updatePost(
+                        id,
+                        req.user.id,
+                        {
+                            title,
+                            content,
+                            categoryIds
+                        }
+                    )
+
+                    res.json({
+                        message: 'Post updated'
+                    })
+
+                } catch (err) {
+                    next(err);
+                }
+
+            });
+
+        this.router.delete('/:id',
+            user,
+            checkAuthValidity,
+            async (req, res, next) => {
+
+                const id = req.params.id;
+
+                try {
+
+                    await  this.postsService.deletePost(id, req.user?.id);
+
+                    res.json({
+                        message: 'Post deleted'
+                    })
+
+                } catch (err) {
+                    next(err);
+                }
+            });
+
+        this.router.delete('/:id/like',
+            user,
+            checkAuthValidity,
+            async (req, res, next) => {
+
+                const id = req.params.id;
+
+                try {
+
+                    await this.postsService.deleteLike(id, req.user?.id);
+                    res.json({
+                        message: 'Like deleted'
+                    })
+                } catch (err) {
+                    next(err);
+                }
+
+            });
+
+        this.router.get('/:id/toggleVisibility',
+            user,
+            checkAuthValidity,
+            admin,
+            async (req, res, next) => {
+
+                const id = req.params.id;
+                const userRole = req.isAdmin ? 'admin' : 'user';
+
+                try {
+
+                    const newStatus = await this.postsService.togglePostVisibility(id, userRole, req.user?.id);
+
+                    res.json({
+                        message: `Post with id: ${id} now is ${newStatus}`
+                    })
+
+                } catch (err) {
+                    next(err);
+                }
+
+            });
+
+    }
+
+    async getPosts(req, res, next) {
+        const page = req.query.page || 0;
+        const userRole = req.isAdmin ? 'admin' : 'user';
+
+        try {
+            res.json(
+                {
+                    posts: await this.postsService.getAll(Number(page), userRole, req.user?.id || -1)
+                }
+            )
+        } catch (err) {
+            next(err);
+        }
     }
 
 
